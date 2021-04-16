2a91f6dba1ea1c11b2ef7c644c0db33c4e90e92d
Babuperumana <babuperumana@gmail.com>
2021-04-16 17:53:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,97 @@
+const fs = require('fs');
+const https = require('https');
+const express = require("express");
+const Sessions = require("./sessions");
+const routes = require('./routes');
+const cors = require('cors');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+require('dotenv').config();
+
+const app = express();
+
+const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || 5000
+
+app.use(express.static('static'));
+app.use(cors());
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use(session({
+  key: 'user_sid',
+  secret: 'somerandonstuffs',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    expires: 600000
+  }
+}));
+
+app.use((req, res, next) => {
+  if (req.cookies.user_sid && !req.session.user) {
+    res.clearCookie('user_sid');
+  }
+  next();
+});
+
+const sessionChecker = (req, res, next) => {
+  if (req.session.user && req.cookies.user_sid) {
+      res.redirect('/login');
+  } else {
+    next();
+  }
+};
+
+app.get('/', sessionChecker, (req, res) => {
+  res.redirect('/login');
+});
+
+app.get('/login', (req, res) => res.sendFile(path.join(__dirname+'/templates/index.html')));
+app.get('/register', (req, res) => res.sendFile(path.join(__dirname+'/templates/register.html')));
+app.get('/testing', (req, res) => res.sendFile(path.join(__dirname+'/templates/testing.html')));
+app.get('/sendlist', (req, res) => res.sendFile(path.join(__dirname+'/templates/send_list.html')));
+app.get('/zapzap', (req, res) => res.sendFile(path.join(__dirname+'/templates/zapzap.html')));
+app.use('/api', routes);
+
+if (process.env.HTTPS === 1) {
+  https.createServer(
+    {
+      key: fs.readFileSync(process.env.SSL_KEY_PATH),
+      cert: fs.readFileSync(process.env.SSL_CERT_PATH)
+    },
+    app).listen(PORT, HOST);
+  console.log("Https server running on port " + 'https://' + HOST + ':' + PORT);
+} else {
+  app.listen(PORT, HOST,() => {
+    console.log("Http server running on port " + 'http://' + HOST + ':' + PORT);
+  });
+}
+
+process.stdin.resume();
+
+async function exitHandler(options, exitCode) {
+  if (options.cleanup) {
+    console.log('cleanup');
+    await Sessions.getSessions().forEach(session => {
+        Sessions.closeSession(session.sessionName);
+    });
+  }
+  if (exitCode || exitCode === 0) {
+    console.log(exitCode);
+  }
+
+  if (options.exit) {
+    process.exit();
+  }
+}
+
+process.on('exit', exitHandler.bind(null, { cleanup: true }));
+process.on('SIGINT', exitHandler.bind(null, { exit: true }));
+process.on('SIGUSR1', exitHandler.bind(null, { exit: true }));
+process.on('SIGUSR2', exitHandler.bind(null, { exit: true }));
+process.on('uncaughtException', exitHandler.bind(null, { exit: true }));
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,318 @@
+const { Router } = require('express');
+const Sessions = require("../sessions");
+const fs = require('fs');
+const real_path = require('path');
+const exec = require("child_process");
+const os = require ('os');
+
+//const bcrypt = require("bcrypt-nodejs");
+//const jwt = require('jsonwebtoken');
+//const passport = require('passport');
+//require('../configurations/password')(passport);
+//const controllers = require('../controllers');
+
+function sleep(millis) {
+  return new Promise(resolve => setTimeout(resolve, millis));
+}
+
+function execute(command){
+  let version = exec.execSync(command);
+  return version.toString();
+}
+
+function update_last_version(){
+  let local_version = parseInt(execute("npm list -l --depth=0 | awk -F@ '/venom-bot/ { print $2}'").split('\n')[0].split('.').join(''));
+  let remote_version = parseInt(execute("npm show venom-bot version").split('\n')[0].split('.').join(''));
+  if (local_version < remote_version) {
+    console.log('OUT OF DATE VERSION.');
+    console.log('UPDATING VERSION TO ' + execute("npm show venom-bot version").split('\n')[0] + '.');
+    //return execute("npm update venom-bot");
+    execute("npm install venom-bot@" + remote_version);
+    execute("npm fund");
+  }else{
+    console.log('IT IS ALREADY WITH THE LATEST VERSION.');
+  }
+}
+
+let platform = os.platform();
+
+if (platform === 'linux') {
+    console.log("you are on a Linux os");
+    update_last_version();
+}else if(platform === 'win32'){
+    console.log("you are on a Windows os")
+}else{
+    console.log("unknown os")
+}
+
+
+const router = Router();
+
+const User = require('../models/user');
+
+function delete_token(path){
+  if(fs.existsSync(path)) {
+    fs.rmdirSync(path, {recursive: true})
+  }else{
+    try {
+      fs.unlinkSync(path);
+    } catch(err) {
+      console.error(err)
+    }
+  }
+
+}
+
+router.post('/user/save', function(req, res) {
+  if (!req.body.username || !req.body.password) {
+    res.status(400).send({
+      status: false,
+      message: ''
+    });
+  } else {
+    User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+      session_key: req.body.username + '-' + getRandomString(10),
+
+      //first_name: req.body.first_name,
+      //last_name: req.body.last_name,
+      //active: req.body.active,
+      //admin: req.body.admin
+
+    }).then((user) => res.status(201).send(user)).catch((error) => {
+      res.status(400).send(error);
+    });
+  }
+
+  function getRandomString(length) {
+    let randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for ( let i = 0; i < length; i++ ) {
+      result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+    }
+    return result;
+  }
+});
+
+router.post('/user/login', function (req, res) {
+  async function check_user() {
+    const user = await User.findOne({where: {username: req.body.username}});
+    let path = real_path.join('./tokens/' + user.session_key + '.data.json')
+    let is_logged = false;
+    if (user) {
+      if (fs.existsSync(path)) {
+        is_logged = true;
+        await Sessions.start(req.query.sessionName);
+      }
+    }
+
+    if (!user) {
+      return res.json({success: false, message: "User does not exist!"});
+    } else {
+      User.comparePassword(req.body.password, user, (err, isMatch) =>{
+        if (!isMatch){
+          return res.json({success: false, message: "Invalid password!!!"});
+        }
+        return res.json({sucess: true, message: "Login successfully.", key: user.session_key, user_id: user.id, is_logged: is_logged});
+      });
+
+      //const passwordHash = bcrypt.compare(req.body.password, user.password, function(err, res) {
+        //if(res) {
+          //console.log('Your password mached with database hash password');
+        //} else {
+          //console.log('Your password not mached.');
+        //}
+      //});
+
+      //console.log(passwordHash + '  ' + user.password)
+      //if (passwordHash === user.password) {
+        //res.send('You are logged in!');
+      //} else {
+        //console.log('incorrect password');
+      //}
+    }
+  }
+
+  check_user().then(r => {});
+
+  /*user.generateToken((err, user)=>{
+    if(err) return res.status(400).json({error: err});
+    res.cookie("x-auth", user.token).status(200).json({
+        message:"Login Success"
+    })
+  })*/
+})
+
+router.post('/user/delete', async function (req, res) {
+  await Sessions.closeSession(req.body.sessionName);
+  try{
+      delete_token(real_path.join('./tokens/' + req.body.sessionName + '.data.json'));
+  }catch (e) {
+    //console.log(e);
+  }
+  User.destroy({
+    where: {
+      id: req.body.user_id
+    }
+  }).then(function (deletedRecord) {
+    console.log(deletedRecord);
+    if(deletedRecord === 1){
+      res.status(200).json({success: true, message:"User removed successfully !!!"});
+    } else {
+      res.status(404).json({success: false, message:"Error removing user."})
+    }
+  }).catch(function (error) {
+    res.status(500).json(error);
+  })
+})
+
+router.get('/database/reset', function (req, res) {
+  try{
+    delete_token(real_path.join('./tokens'));
+  }catch (e) {
+    //console.log(e);
+  }
+  User.destroy({
+    where: {
+    }
+  }).then(function (deletedRecord) {
+    if(deletedRecord){
+      res.status(200).json({success: true, message:"Database successfully reset !!!"});
+    } else {
+      res.status(404).json({success: false, message:"Error when resetting database."})
+    }
+  }).catch(function (error) {
+    res.status(500).json(error);
+  })
+})
+
+router.get('/database/load', function (req, res) {
+  User.findAll().then(function (load_data) {
+    if(load_data){
+      res.status(200).json({success: true, object: load_data, message:"Database loaded successfully !!!"});
+    } else {
+      res.status(404).json({success: false, message:"Error loading database."})
+    }
+  }).catch(function (error) {
+    res.status(500).json(error);
+  })
+})
+
+router.get("/start", async (req, res, next) => {
+  console.log("starting..." + req.query.sessionName);
+  const session = await Sessions.start(req.query.sessionName);
+  if (["UNPAIRED", "UNPAIRED_IDLE"].includes(session.state)){
+    delete_token(real_path.join('./tokens/' + req.body.sessionName + '.data.json'));
+  }
+  else if (["CONNECTED", "QRCODE", "STARTING"].includes(session.state)) {
+    res.redirect("../api/qrcode?sessionName=" + req.query.sessionName + '&image=true')
+  } else {
+    res.status(200).json({ result: 'error', message: session.state });
+  }
+  return res.end();
+});
+
+router.get("/new_qrcode", async function (req, res, next) {
+  const session = await Sessions.getSession(req.query.sessionName);
+  if (session) {
+
+    console.log('LOOK AT THE STATUS: ' + session.status + ' AND LOOK AT THE STATE: ' + session.state);
+
+    if (session.status === 'qrReadSuccess'){
+      return res.json({success: 'true', object: false, message: 'You are already logged in!!!', is_active: true});
+    }
+    if (session.status === 'isLogged' || session.status === 'inChat'){
+      //res.redirect('/testing');
+      return res.json({success: 'true', object: false, message: 'You are already logged in!!!', is_active: true});
+    }else if (session.status === 'notLogged' && session.state !== 'STARTING') {
+      res.json({success: 'true', object: session.qrcode, message: 'New qrcode successfully generated!!!', is_active: false});
+    }
+  } else {
+    console.log('NO SESSION ... ')
+    res.json({success: "false", message: "NOTFOUND" });
+  }
+});
+
+router.get("/qrcode", async (req, res, next) => {
+  console.log("qrcode..." + req.query.sessionName);
+  const session = Sessions.getSession(req.query.sessionName);
+  if (session !== false) {
+    if (session.status === 'notLogged') {
+      if (req.query.image) {
+        res.redirect('/zapzap');
+      } else {
+        res.status(200).json({result: "success", message: session.state, qrcode: session.qrcode});
+      }
+    } else if (session.status === 'inChat' || session.status === 'isLogged') {
+      res.redirect('/testing');
+    }
+  } else {
+    res.status(200).json({ result: "error", message: session.state });
+  }
+});
+
+router.post("/sendText", async function sendText(req, res, next) {
+  const result = await Sessions.sendText(
+    req.body.sessionName,
+    req.body.number,
+    req.body.text
+  );
+  res.json(result);
+});
+
+router.post("/send_list", async function sendText(req, res, next) {
+  let list_req = req.body.numbers
+  let number = null;
+  let result = null;
+  for ( let i = 0; i < list_req.length; i++ ) {
+    number = '55' + list_req[i];
+
+    console.log('SENDING...' + number);
+
+    result = await Sessions.sendText(
+      req.body.sessionName,
+      number,
+      req.body.text
+    );
+
+    await sleep(10000);
+  }
+  res.json(result);
+});
+
+router.get("/sendText", async (req, res, next) => {
+  const result = await Sessions.sendText(
+    req.query.sessionName,
+    req.query.number,
+    req.query.text
+  );
+
+  console.log(result);
+
+  res.json(result);
+});
+
+router.post("/sendFile", async (req, res, next) => {
+  const result = await Sessions.sendFile(
+    req.body.sessionName,
+    req.body.number,
+    req.body.base64Data,
+    req.body.fileName,
+    req.body.caption
+  );
+  res.json(result);
+});
+
+router.get("/messages/unread", async function (req, res, next) {
+  const result = await Sessions.unreadMessages(req.query.sessionName);
+  res.json(result);
+});
+
+router.get("/close", async (req, res, next) => {
+  const result = await Sessions.closeSession(req.query.sessionName);
+  res.json(result);
+});
+
+module.exports = router;
